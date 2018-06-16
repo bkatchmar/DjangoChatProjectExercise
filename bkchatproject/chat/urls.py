3272eb69984dabfbc04dbea4e38fdb1ba6348075
Brian Katchmar <bkatchmar@gmail.com>
2018-06-16 11:00:43 -0400

    Able to create new channels and edit your display name, still gotta put in the like mechanic

--- bkchatproject/chat/urls.py
+++ bkchatproject/chat/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 app_name = "chat"
 urlpatterns = [
+    url(r'^my-settings', views.MySettingsView.as_view(), name="my_settings"),
+    url(r'^my-rooms', views.MyChatRoomsView.as_view(), name="my_chats"),
     url(r'^all-rooms', views.AllChatRoomsView.as_view(), name="all_chats"),
-    url(r'^room/(?P<contract_id>[0-9]+)', views.ChatHomeView.as_view(), name="specific_room"),
+    url(r'^room/(?P<chat_id>[0-9]+)', views.ChatHomeView.as_view(), name="specific_room"),
     url(r'^', views.ChatHomeView.as_view(), name="home"),
 ]
--- bkchatproject/chat/views.py
+++ bkchatproject/chat/views.py
@@ -1,7 +1,8 @@
 from accounts.models import UserSettings
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.utils import timezone
+from django.urls import reverse
 from django.views.generic import TemplateView
 from chat.models import ChatRoom, Message, UserChatRoom
 
@@ -9,11 +10,11 @@ class ChatHomeView(LoginRequiredMixin, TemplateView):
     template_name = "chat.home.html"
     
     def get(self, request, **kwargs):
-        context = self.get_context_data(request)
+        context = self.get_context_data(request, **kwargs)
         return render(request, self.template_name, context)
     
     def post(self, request, **kwargs):
-        context = self.get_context_data(request)
+        context = self.get_context_data(request, **kwargs)
         
         # Get request data sent by user
         message_body = request.POST.get("message-body", "")
@@ -25,48 +26,46 @@ class ChatHomeView(LoginRequiredMixin, TemplateView):
                 MessageDateTime=timezone.now(),
                 MessageText=message_body
             )
-            context["chat_messages"].insert
-            (
-                0,
-                {
-                    "username" : context["user_settings"].DisplayName,
-                    "text" : new_message.MessageText,
-                    "datetime" : new_message.MessageDateTime.strftime("%b %d %Y %H:%M"),
-                    "Liked" : False
-                }
-            )
+            context["chat_messages"].insert(0,{
+                "username" : context["user_settings"].DisplayName,
+                "text" : new_message.MessageText,
+                "datetime" : new_message.MessageDateTime.strftime("%b %d %Y %H:%M"),
+                "liked" : False})
         
         return render(request, self.template_name, context)
     
     def get_context_data(self, request, **kwargs):
         context = super(ChatHomeView, self).get_context_data(**kwargs)
         
-        # General check to see if the chat "General" event exists, all users go into this chat by default
-        # TO DO: Make General Default, when we're ready to do more chat rooms, will re-factor this
+        # Base General Chat Check
         if not ChatRoom.objects.filter(DisplayName="General").exists():
-            general_chat = ChatRoom.objects.create(DisplayName="General")
-        else:
-            general_chat = ChatRoom.objects.get(DisplayName="General")
+            ChatRoom.objects.create(DisplayName="General")
+        
+        # Get the chat information requested from the request
+        if "chat_id" in kwargs:
+            selected_chat = ChatRoom.objects.filter(id=kwargs.get("chat_id")).first()
             
+            if selected_chat is None:
+                selected_chat = ChatRoom.objects.get(DisplayName="General")
+        else:
+            selected_chat = ChatRoom.objects.get(DisplayName="General")
+        
         # Check if user is a part of General Chat, if not, add them
-        if not UserChatRoom.objects.filter(UserAccount=request.user,ChatRoomForUser=general_chat).exists():
-            UserChatRoom.objects.create(UserAccount=request.user,ChatRoomForUser=general_chat)
+        if not UserChatRoom.objects.filter(UserAccount=request.user,ChatRoomForUser=selected_chat).exists():
+            UserChatRoom.objects.create(UserAccount=request.user,ChatRoomForUser=selected_chat)
         
         # Get all messages in this chat
-        chat_messages = Message.objects.filter(ChatRoomForMessage=general_chat).order_by("-MessageDateTime")
+        chat_messages = Message.objects.filter(ChatRoomForMessage=selected_chat).order_by("-MessageDateTime")
         context["chat_messages"] = []
-        context["chat_room"] = general_chat
+        context["chat_room"] = selected_chat
         context["user_settings"] = UserSettings.objects.get(UserAccount=request.user)
         
         for msg in chat_messages:
-            context["chat_messages"].append(
-                {
-                    "username" : msg.UserThatPosted.DisplayName, 
-                    "text" : msg.MessageText, 
-                    "datetime" : msg.MessageDateTime.strftime("%b %d %Y %H:%M"), 
-                    "Liked" : False 
-                }
-            )
+            context["chat_messages"].append({
+                "username" : msg.UserThatPosted.DisplayName, 
+                "text" : msg.MessageText,
+                "datetime" : msg.MessageDateTime.strftime("%b %d %Y %H:%M"), 
+                "liked" : False})
         
         return context
 
@@ -80,4 +79,71 @@ class AllChatRoomsView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super(AllChatRoomsView, self).get_context_data(**kwargs)
         context["chat_rooms"] = ChatRoom.objects.all()
+        return context
+    
+class MyChatRoomsView(LoginRequiredMixin, TemplateView):
+    template_name = "chat.my.html"
+    
+    def get(self, request, **kwargs):
+        context = self.get_context_data(request)
+        return render(request, self.template_name, context)
+    
+    def post(self, request, **kwargs):
+        error_msg = ""
+        
+        # Get information from the request
+        new_chat_name = request.POST.get("new-chat-name", "")
+        
+        # Some Base Validation Handle
+        if new_chat_name == "":
+            error_msg = "Name Cannot Be Empty"
+        else:
+            new_chat = ChatRoom.objects.filter(DisplayName=new_chat_name).first()
+            
+            if new_chat is None:
+                new_chat = ChatRoom.objects.create(DisplayName=new_chat_name)
+            
+            UserChatRoom.objects.create(UserAccount=request.user,ChatRoomForUser=new_chat)
+        
+        context = self.get_context_data(request)
+        context["error_msg"] = error_msg
+        return render(request, self.template_name, context)
+    
+    def get_context_data(self, request, **kwargs):
+        context = super(MyChatRoomsView, self).get_context_data(**kwargs)
+        context["chat_rooms"] = UserChatRoom.objects.filter(UserAccount=request.user)
+        return context
+    
+class MySettingsView(LoginRequiredMixin, TemplateView):
+    template_name = "user.settings.html"
+    
+    def get(self, request, **kwargs):
+        context = self.get_context_data(request)
+        return render(request, self.template_name, context)
+    
+    def post(self, request, **kwargs):
+        context = self.get_context_data(request)
+        return render(request, self.template_name, context)
+    
+    def post(self, request, **kwargs):
+        error_msg = ""
+        user_settings = UserSettings.objects.get(UserAccount=request.user)
+        
+        # Get information from the request
+        new_user_name = request.POST.get("new-user-name", "")
+        
+        # Some Base Validation Handle
+        if new_user_name == "":
+            error_msg = "Name Cannot Be Empty"
+        else:
+            user_settings.DisplayName = new_user_name
+            user_settings.save()
+            return redirect(reverse("chat:home"))
+        
+        context = self.get_context_data(request)
+        context["error_msg"] = error_msg
+        return render(request, self.template_name, context)
+    
+    def get_context_data(self, request, **kwargs):
+        context = super(MySettingsView, self).get_context_data(**kwargs)
         return context
