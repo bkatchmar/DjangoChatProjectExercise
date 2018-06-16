97356c2f90a7d6b69964e7bbd2c2f234c5a2e481
Brian Katchmar <bkatchmar@gmail.com>
2018-06-16 02:30:32 -0400

    Getting in the models needed for having a chat feature up and running, I gotta refactor things but I really need to get to bed

--- bkchatproject/chat/models.py
+++ bkchatproject/chat/models.py
@@ -1,3 +1,32 @@
+from accounts.models import UserSettings
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class ChatRoom(models.Model):
+    DisplayName = models.CharField(max_length=50,null=False)
+    
+    class Meta:
+       db_table = "ChatRoom"
+       
+class UserChatRoom(models.Model):
+    UserAccount = models.ForeignKey(User,on_delete=models.CASCADE)
+    ChatRoomForUser = models.ForeignKey(ChatRoom,on_delete=models.CASCADE)
+    
+    class Meta:
+       db_table = "UserChatRoom"
+       
+class Message(models.Model):
+    UserThatPosted = models.ForeignKey(UserSettings,null=False,on_delete=models.CASCADE)
+    ChatRoomForMessage = models.ForeignKey(ChatRoom,null=False,on_delete=models.CASCADE)
+    MessageDateTime = models.DateTimeField(auto_now=False,auto_now_add=False,null=False)
+    MessageText = models.TextField(null=False)
+    
+    class Meta:
+       db_table = "Message"
+
+class MessageLike(models.Model):
+    MessageLiked = models.ForeignKey(Message,null=False,on_delete=models.CASCADE)
+    UserWhoLiked = models.ForeignKey(UserSettings,null=False,on_delete=models.CASCADE)
+    
+    class Meta:
+       db_table = "MessageLike"
--- bkchatproject/chat/views.py
+++ bkchatproject/chat/views.py
@@ -1,18 +1,71 @@
+from accounts.models import UserSettings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import TemplateView
+from chat.models import ChatRoom, Message, UserChatRoom
 
 class ChatHomeView(LoginRequiredMixin, TemplateView):
     template_name = "chat.home.html"
     
-    def get(self, request):
-        context = self.get_context_data()
+    def get(self, request, **kwargs):
+        context = self.get_context_data(request)
         return render(request, self.template_name, context)
     
-    def post(self, request):
-        context = self.get_context_data()
+    def post(self, request, **kwargs):
+        context = self.get_context_data(request)
+        
+        # Get request data sent by user
+        message_body = request.POST.get("message-body", "")
+        
+        if message_body != "":
+            new_message = Message.objects.create(
+                UserThatPosted=context["user_settings"],
+                ChatRoomForMessage=context["chat_room"],
+                MessageDateTime=timezone.now(),
+                MessageText=message_body
+            )
+            context["chat_messages"].insert
+            (
+                0,
+                {
+                    "username" : context["user_settings"].DisplayName,
+                    "text" : new_message.MessageText,
+                    "datetime" : new_message.MessageDateTime.strftime("%b %d %Y %H:%M"),
+                    "Liked" : False
+                }
+            )
+        
         return render(request, self.template_name, context)
     
-    def get_context_data(self, **kwargs):
-        context = super(ChatHomeView, self).get_context_data(**kwargs)        
+    def get_context_data(self, request, **kwargs):
+        context = super(ChatHomeView, self).get_context_data(**kwargs)
+        
+        # General check to see if the chat "General" event exists, all users go into this chat by default
+        # TO DO: Make General Default, when we're ready to do more chat rooms, will re-factor this
+        if not ChatRoom.objects.filter(DisplayName="General").exists():
+            general_chat = ChatRoom.objects.create(DisplayName="General")
+        else:
+            general_chat = ChatRoom.objects.get(DisplayName="General")
+            
+        # Check if user is a part of General Chat, if not, add them
+        if not UserChatRoom.objects.filter(UserAccount=request.user,ChatRoomForUser=general_chat).exists():
+            UserChatRoom.objects.create(UserAccount=request.user,ChatRoomForUser=general_chat)
+        
+        # Get all messages in this chat
+        chat_messages = Message.objects.filter(ChatRoomForMessage=general_chat).order_by("-MessageDateTime")
+        context["chat_messages"] = []
+        context["chat_room"] = general_chat
+        context["user_settings"] = UserSettings.objects.get(UserAccount=request.user)
+        
+        for msg in chat_messages:
+            context["chat_messages"].append(
+                {
+                    "username" : msg.UserThatPosted.DisplayName, 
+                    "text" : msg.MessageText, 
+                    "datetime" : msg.MessageDateTime.strftime("%b %d %Y %H:%M"), 
+                    "Liked" : False 
+                }
+            )
+        
         return context
