c318698687b9c3803628f736eee51708211c12b4
Brian Katchmar <bkatchmar@gmail.com>
2018-06-16 13:20:22 -0400

    Final Pass minus refactors and improvements
    I’d like to add more AngualrJS validation and unit tests, but the core
    functionality asked for is in here

--- bkchatproject/chat/urls.py
+++ bkchatproject/chat/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 app_name = "chat"
 urlpatterns = [
+    url(r'^api/edit-message/(?P<mesage_id>[0-9]+)', views.JsonEditMessage.as_view(), name='json_edit_message'),
+    url(r'^api/like-message/(?P<mesage_id>[0-9]+)', views.JsonLikeMessage.as_view(), name='json_like_message'),
+    url(r'^my-liked-messages', views.MyLikedMessages.as_view(), name="my_liked_messages"),
     url(r'^my-settings', views.MySettingsView.as_view(), name="my_settings"),
     url(r'^my-rooms', views.MyChatRoomsView.as_view(), name="my_chats"),
     url(r'^all-rooms', views.AllChatRoomsView.as_view(), name="all_chats"),
--- bkchatproject/chat/views.py
+++ bkchatproject/chat/views.py
@@ -1,10 +1,13 @@
+import json
+
 from accounts.models import UserSettings
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.utils import timezone
 from django.urls import reverse
-from django.views.generic import TemplateView
-from chat.models import ChatRoom, Message, UserChatRoom
+from django.views.generic import TemplateView, View
+from chat.models import ChatRoom, Message, MessageLike, UserChatRoom
 
 class ChatHomeView(LoginRequiredMixin, TemplateView):
     template_name = "chat.home.html"
@@ -27,10 +30,12 @@ class ChatHomeView(LoginRequiredMixin, TemplateView):
                 MessageText=message_body
             )
             context["chat_messages"].insert(0,{
+                "id" : new_message.id,
                 "username" : context["user_settings"].DisplayName,
                 "text" : new_message.MessageText,
                 "datetime" : new_message.MessageDateTime.strftime("%b %d %Y %H:%M"),
-                "liked" : False})
+                "liked" : False,
+                "can_edit" : True})
         
         return render(request, self.template_name, context)
     
@@ -56,16 +61,21 @@ class ChatHomeView(LoginRequiredMixin, TemplateView):
         
         # Get all messages in this chat
         chat_messages = Message.objects.filter(ChatRoomForMessage=selected_chat).order_by("-MessageDateTime")
+        user_settings = UserSettings.objects.get(UserAccount=request.user)
+        all_liked_messages = MessageLike.objects.all()
+        
         context["chat_messages"] = []
         context["chat_room"] = selected_chat
-        context["user_settings"] = UserSettings.objects.get(UserAccount=request.user)
+        context["user_settings"] = user_settings
         
         for msg in chat_messages:
             context["chat_messages"].append({
+                "id" : msg.id,
                 "username" : msg.UserThatPosted.DisplayName, 
                 "text" : msg.MessageText,
                 "datetime" : msg.MessageDateTime.strftime("%b %d %Y %H:%M"), 
-                "liked" : False})
+                "liked" : all_liked_messages.filter(MessageLiked=msg,UserWhoLiked=user_settings).exists(),
+                "can_edit" : (msg.UserThatPosted.id == user_settings.id)})
         
         return context
 
@@ -122,10 +132,6 @@ class MySettingsView(LoginRequiredMixin, TemplateView):
         return render(request, self.template_name, context)
     
     def post(self, request, **kwargs):
-        context = self.get_context_data(request)
-        return render(request, self.template_name, context)
-    
-    def post(self, request, **kwargs):
         error_msg = ""
         user_settings = UserSettings.objects.get(UserAccount=request.user)
         
@@ -147,3 +153,51 @@ class MySettingsView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, request, **kwargs):
         context = super(MySettingsView, self).get_context_data(**kwargs)
         return context
+
+class MyLikedMessages(LoginRequiredMixin, TemplateView):
+    template_name = "user.liked.messages.html"
+    
+    def get(self, request, **kwargs):
+        context = self.get_context_data(request)
+        return render(request, self.template_name, context)
+    
+    def get_context_data(self, request, **kwargs):
+        user_settings = UserSettings.objects.get(UserAccount=request.user)
+        
+        context = super(MyLikedMessages, self).get_context_data(**kwargs)
+        context["messages"] = MessageLike.objects.filter(UserWhoLiked=user_settings)
+        return context
+
+class JsonLikeMessage(LoginRequiredMixin, View):
+    def get(self, request, **kwargs):
+        data = { "success" : True }
+        
+        selected_message = Message.objects.filter(id=kwargs.get("mesage_id")).first()
+        user_settings = UserSettings.objects.get(UserAccount=request.user)
+        
+        if selected_message is None:
+            data["success"] = False
+        else:
+            message_to_like = MessageLike.objects.filter(MessageLiked=selected_message,UserWhoLiked=user_settings).first()
+            if message_to_like is None:
+                MessageLike.objects.create(MessageLiked=selected_message,UserWhoLiked=user_settings)
+            else:
+                message_to_like.delete()
+        
+        return JsonResponse(data)
+
+class JsonEditMessage(LoginRequiredMixin, View):
+    def post(self, request, **kwargs):
+        data = { "success" : True }
+        request_body = json.loads(request.body.decode('utf-8'))
+        
+        selected_message = Message.objects.filter(id=kwargs.get("mesage_id")).first()
+        new_message_text = request_body["text"]
+        
+        if selected_message is None:
+            data["success"] = False
+        else:
+            selected_message.MessageText = new_message_text
+            selected_message.save()
+        
+        return JsonResponse(data)
